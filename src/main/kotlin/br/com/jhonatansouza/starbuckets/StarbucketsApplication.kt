7e093fc11e39093a481195afca4b3793175bedfe
jhonatan2760 <jhonatan2760@gmail.com>
2020-12-06 09:31:51 -0300

    Setting Kotlin project

---
+++ src/main/kotlin/br/com/jhonatansouza/starbuckets/StarbucketsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jhonatansouza.starbuckets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarbucketsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarbucketsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jhonatansouza/starbuckets/StarbucketsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jhonatansouza.starbuckets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarbucketsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
