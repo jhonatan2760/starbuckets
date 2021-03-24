6711330dd7265cdb9381a9a8daabb7f5129e8999
Jhonatan S. Souza <jhonatan2760@gmail.com>
2021-03-24 19:41:27 -0300

    Criação de massa.

--- src/main/kotlin/br/com/jhonatansouza/starbuckets/controller/ProductController.kt
+++ src/main/kotlin/br/com/jhonatansouza/starbuckets/controller/ProductController.kt
@@ -8,6 +8,13 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
+import java.util.*
+import java.util.function.BiConsumer
+import java.util.function.Consumer
+import java.util.function.Supplier
+import java.util.stream.Collectors
+import java.util.stream.Stream
+import kotlin.collections.HashSet
 
 @RestController
 @RequestMapping("api/product/v1")
@@ -47,5 +54,18 @@ class ProductController(private var service: ProductService) {
         return ResponseEntity.ok(service.update(id, product))
     }
 
+    @PostMapping("/generate")
+    fun createData(): ResponseEntity<String> {
 
+        val item = Arrays.asList("Café tipo", "Leite tipo", "Vitamina", "Sobremesa", "água de", "Leite de", "Sorvete de", "Pão de");
+        val recheio = Arrays.asList("Expresso", "Morango", "Beterraba", "Laranja", "Chocolate", "Uva", "Açucar", "Mamão", "Suspiro", "Agridoce");
+
+        val itens = Stream.generate(Supplier { Math.random() })
+                .map { "${item[(Math.random() * item.size).toInt()]} - ${recheio[(Math.random() * recheio.size).toInt()]}" }
+                .limit(600)
+                .peek(System.out::println)
+                .collect(Collectors.toSet())
+
+        return ResponseEntity.ok(itens.toString())
+    }
 }
