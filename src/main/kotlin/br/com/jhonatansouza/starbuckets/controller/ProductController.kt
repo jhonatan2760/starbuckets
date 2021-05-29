48c1eed3c56bdb2408779942a753f531e14eb83e
Jhonatan S. Souza <jhonatan2760@gmail.com>
2021-05-29 16:34:07 -0300

    Update configs

--- src/main/kotlin/br/com/jhonatansouza/starbuckets/controller/ProductController.kt
+++ src/main/kotlin/br/com/jhonatansouza/starbuckets/controller/ProductController.kt
@@ -1,49 +1,36 @@
-package br.com.jhonatansouza.starbuckets.controller
+package br.com.jhonatansouza.starbuckets
 
 import br.com.jhonatansouza.starbuckets.model.Product
 import br.com.jhonatansouza.starbuckets.model.request.ProductRequest
 import br.com.jhonatansouza.starbuckets.model.response.ProductResponse
 import br.com.jhonatansouza.starbuckets.service.ProductService
 import org.slf4j.LoggerFactory
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
-@RequestMapping("api/supplier/v1")
-class ProductWithSupplierController(private var service: ProductService) {
+@RequestMapping("api/product/v1/")
+class ProductController(private var service: ProductService) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @PostMapping
-    fun createProductWithSupplier(
-            @RequestBody request: ProductRequest,
-            uri: UriComponentsBuilder
+    fun create(
+        @RequestBody request: ProductRequest,
+        uri: UriComponentsBuilder
     ): ResponseEntity<Any> {
         logger.info("Creating product, productName=${request.name}")
-        return ResponseEntity.created(uri.path("/api/product/v1/{id}")
-                .build(service.create(ProductRequest.toEntity(request)).id)).build()
-    }
-
-    @PostMapping("/generate")
-    fun createData(): ResponseEntity<String> {
-        this.logger.info("create itens pag")
-        val response = service.createData()
-        return ResponseEntity.ok(response.toString())
-    }
-
-    @GetMapping
-    fun getAllProduct(page: Pageable): ResponseEntity<Page<Product>> {
-        this.logger.info("Get all product")
-        return ResponseEntity.ok(service.getAll(page))
+        return ResponseEntity.created(
+            uri.path("/api/product/v1/{id}")
+                .build(service.create(ProductRequest.toProduct(request)).id)
+        ).build()
     }
 
     @GetMapping("/{id}")
-    fun findProductWithSupplier(@PathVariable id: String): ResponseEntity<ProductResponse> {
+    fun findProduct(@PathVariable id: String): ResponseEntity<ProductResponse> {
         logger.info("Finding product, productId=$id")
-        return ResponseEntity.ok(ProductResponse.fromEntity(service.getById(id)))
+        return ResponseEntity.ok(ProductResponse.toResponse(service.getById(id)))
     }
 
     @PutMapping("/{id}")
